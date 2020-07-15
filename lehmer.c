3c460dc1c302dcb9377fe323989021b7fe3f7f2e
joyastockwell <36890358+joyastockwell@users.noreply.github.com>
2020-07-15 10:22:37 -0700

    Update lehmer.c

--- lehmer.c
+++ lehmer.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 
-private double seed = 42;
+private seed = 42;
 private double m = 2147483647;
+// This is the recommended a value from Communications of the ACM, but its square is 2330089441
+// Since 2330089441 > m, we cannot use it with Schrage's method
 private double a = 48271;
+private double schrage_a = 16807;
 
 float float_lehmer_num_btwn_0_and_1() {
   
@@ -32,13 +35,24 @@ long double long_double_lehmer_num_btwn_0_and_1() {
   
 }
 
-long double schrage_lehmer_num_btwn_0_and_1() {
+float schrage_lehmer_num_btwn_0_and_1() {
 
-
+   return 0;
 
 }
 
 int main() {
   
-  printf("Hi, welcome. We will be comparing the performance of various implementations of lehmer's PRNG.")
+  printf("Hi, welcome. We will be comparing the performance of various implementations of Lehmer's PRNG.\n");
+  printf("size of float: ");
+  printf(sizeof(float));
+  printf("\n");;
+  printf("size of double: ");
+  printf(sizeof(double));
+  printf("\n");;
+  printf("size of long double: ");
+  printf(sizeof(long double));
+  printf("\n");
+  
+  
 }
